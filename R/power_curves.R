d183e76432d396cec98013b4f1f2a8a7b90b483e
Lucy <ld.mcgowan@vanderbilt.edu>
2017-09-25 18:11:38 -0500

    :see_no_evil: Fix little :bug:s

--- R/power_curves.R
+++ R/power_curves.R
@@ -44,7 +44,7 @@ power_curves <- function(n, delta, truth = "null") {
   theta <- seq(-3, 3, 0.001)
   if (truth == "null") {
     p <- purrr::map(delta, power_null, n = n, theta = theta)
-    plot(theta,
+    graphics::plot(theta,
          p[[1]],
          type = "n",
          ylim = c(0, 1),
@@ -52,14 +52,14 @@ power_curves <- function(n, delta, truth = "null") {
          xlab = "Alternative (SD units)",
          ylab="Probability")
     for (i in seq_along(p)) {
-      lines(theta, p[[i]], lty = 1, lwd = 1, col = i)
+      graphics::lines(theta, p[[i]], lty = 1, lwd = 1, col = i)
     }
-    abline(h = 0.05, lwd = 0.5, lty = 2)
+    graphics::abline(h = 0.05, lwd = 0.5, lty = 2)
   }
 
   if (truth == "alternative") {
     p <- purrr::map(delta, power_alt, n = n, theta = theta)
-    plot(theta,
+    graphics::plot(theta,
          p[[1]],
          type = "n",
          ylim = c(0, 1),
@@ -67,17 +67,17 @@ power_curves <- function(n, delta, truth = "null") {
          xlab = "Alternative (SD units)",
          ylab = "Probability")
     for (i in seq_along(p)) {
-      lines(theta, p[[i]], lty = 1, lwd = 1, col = i)
+      graphics::lines(theta, p[[i]], lty = 1, lwd = 1, col = i)
     }
-    abline(h = 0.05, lty = 2)
+    graphics::abline(h = 0.05, lty = 2)
   }
 
   if (truth == "inconclusive") {
     p_weak <- purrr::map(delta, power_inconclusive, n = n, theta = theta)
 
-    par(yaxs = "i")
+    graphics::par(yaxs = "i")
 
-    plot(theta,
+    graphics::plot(theta,
          p_weak[[1]],
          type = "n",
          ylim = c(0, length(p_weak)),
@@ -86,36 +86,36 @@ power_curves <- function(n, delta, truth = "null") {
          ylab = "Probability",
          yaxt = "n")
 
-    axis(2,
+    graphics::axis(2,
          at = seq(0, length(p_weak), by = .25),
          label = c(0, rep(c("", .5, "", 0), length(p_weak))),
          las=2)
 
-    abline(h = 0, lty = 2, lwd = 0.5)
+    graphics::abline(h = 0, lty = 2, lwd = 0.5)
     for (i in seq_along(p_weak)) {
-      rect(-delta[i],
+      graphics::rect(-delta[i],
            (length(p_weak) - i + 1),
            delta[i],
            length(p_weak) - i,
-           col = rgb(1,0.9,1,0.6),
+           col = grDevices::rgb(1,0.9,1,0.6),
            border = F)
-      abline(h = i, lty = 2, lwd = 0.5)
-      lines(theta, p_weak[[i]] + (length(p_weak) - i), lty = 1, lwd = 1, col = i)
-      text(1.6, length(p_weak) - i + 0.5, bquote(delta~"="~.(round(delta[i], 2))))
+      graphics::abline(h = i, lty = 2, lwd = 0.5)
+      graphics::lines(theta, p_weak[[i]] + (length(p_weak) - i), lty = 1, lwd = 1, col = i)
+      graphics::text(1.6, length(p_weak) - i + 0.5, bquote(delta~"="~.(round(delta[i], 2))))
     }
   }
 }
 
 ## Power under the null
 power_null <- function(delta, n, theta) {
-  pnorm(sqrt(n) * (-delta) - sqrt(n) * theta - 1.96) +
-    pnorm(-sqrt(n) * delta + sqrt(n) * theta - 1.96)
+  stats::pnorm(sqrt(n) * (-delta) - sqrt(n) * theta - 1.96) +
+    stats::pnorm(-sqrt(n) * delta + sqrt(n) * theta - 1.96)
 }
 
 ## Power under the alternative
 power_alt <- function(delta, n, theta) {
-  p <- pnorm(sqrt(n) * delta - sqrt(n) * theta - 1.96) -
-    pnorm(sqrt(n) * (-delta) - sqrt(n) * theta + 1.96)
+  p <- stats::pnorm(sqrt(n) * delta - sqrt(n) * theta - 1.96) -
+    stats::pnorm(sqrt(n) * (-delta) - sqrt(n) * theta + 1.96)
 
   if (delta <= 1.96 * sqrt(1 / n)) {
     p <- rep(0, length(theta))
