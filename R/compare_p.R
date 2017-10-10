4cb40bffce9c6d22effaa24cdb8aa8a4f72667f3
Lucy <ld.mcgowan@vanderbilt.edu>
2017-10-10 11:30:03 -0500

    :dancers: Update with Robert's p_delta code

--- R/compare_p.R
+++ R/compare_p.R
@@ -1,8 +1,8 @@
 #' Compare traditional, max, and second generation P-values across studies
 #'
 #' @param data Data frame with two variables `xbar` and `se`.
-#' @param delta_a Numeric. Lower bound of the indifference zone
-#' @param delta_b Numeric. Upper bound of the indifference zone
+#' @param delta_lb Numeric. Lower bound of the indifference zone
+#' @param delta_ub Numeric. Upper bound of the indifference zone
 #' @param h0 Numeric. Value of the null hypothesis
 #' @param plot Logical. Whether to produce a plot of the study values with the
 #'   indifference zone (default = `TRUE`).
@@ -26,19 +26,19 @@
 #'   xbar = c(146, 145.5, 145, 146, 144, 143.5, 142, 141),
 #'   se = c(0.5, 0.25, 1.25, 2.25, 1, 0.5, 1, 0.5)
 #'   )
-#' compare_p(data, delta_a = 144, delta_b = 148, h0 = 146)
-compare_p <- function(data, delta_a, delta_b, h0, plot = TRUE) {
+#' compare_p(data, delta_lb = 144, delta_ub = 148, h0 = 146)
+compare_p <- function(data, delta_lb, delta_ub, h0, plot = TRUE) {
   data$lb <- data$xbar - 1.96 * data$se
   data$ub <- data$xbar + 1.96 * data$se
   z1 <- (data$xbar - h0) / data$se
 
   p_old <- round(2 * stats::pnorm(- abs(z1)), 5)
 
-  z2 <- ifelse(abs(data$xbar - delta_b) < abs(data$xbar - delta_a),
-               (data$xbar - delta_b) / data$se,
-               (data$xbar - delta_a) / data$se)
+  z2 <- ifelse(abs(data$xbar - delta_ub) < abs(data$xbar - delta_lb),
+               (data$xbar - delta_ub) / data$se,
+               (data$xbar - delta_lb) / data$se)
 
-  z2 <- ifelse(data$xbar > delta_b | delta_a > data$xbar,
+  z2 <- ifelse(data$xbar > delta_ub | delta_lb > data$xbar,
                z2,
                0)
 
@@ -47,28 +47,28 @@ compare_p <- function(data, delta_a, delta_b, h0, plot = TRUE) {
   p_new <- purrr::map2_dbl(data$lb,
                            data$ub,
                            p_delta,
-                           delta_a = delta_a,
-                           delta_b = delta_b
+                           delta_lb = delta_lb,
+                           delta_ub = delta_ub
   )
 
   if (plot) {
     graphics::plot(c(data$xbar, NA),
          1:(length(data$xbar) + 1),
-         xlim = c(min(data$lb) - 2, max(data$ub, delta_b)),
+         xlim = c(min(data$lb) - 2, max(data$ub, delta_ub)),
          xaxt = "n",
          yaxt = "n",
          ylab = "",
          xlab = "",
          type = "n")
     graphics::axis(side = 1,
-         at = seq(round(min(data$lb, delta_a)),
-                  round(max(data$ub, delta_b)),
-                  delta_b - h0)
+         at = seq(round(min(data$lb, delta_lb)),
+                  round(max(data$ub, delta_ub)),
+                  delta_ub - h0)
     )
 
-    graphics::rect(delta_a,
+    graphics::rect(delta_lb,
          length(data$xbar) + 1,
-         delta_b,
+         delta_ub,
          0,
          col = grDevices::rgb(208, 216, 232, max = 255),
          border = NA)
--- R/p_delta.R
+++ R/p_delta.R
@@ -2,8 +2,8 @@
 #'
 #' @param lb Numeric. Lower bound of the observed effect
 #' @param ub Numeric. Upper bound of the observed effect
-#' @param delta_a Numeric. Lower bound of the indifference zone
-#' @param delta_b Numeric. Upper bound of the indifference zone
+#' @param delta_lb Numeric. Lower bound of the indifference zone
+#' @param delta_ub Numeric. Upper bound of the indifference zone
 #'
 #' @return Numeric. Second generation p-value for the observed effect.
 #' @export
@@ -12,13 +12,47 @@
 #' ## Second Generation P-value for a confidence interval of (140, 145) and an
 #' ## indifference zone of (144, 148)
 #' p_delta(140, 145, 144, 148)
-p_delta <- function(lb, ub, delta_a, delta_b) {
-  interval <- ub - lb
-  null <- delta_b - delta_a
+p_delta <- function(lb, ub, delta_lb, delta_ub) {
 
-  intersect <- min(delta_b, max(ub, delta_a)) - max(delta_a, min(lb, delta_b))
+  # special case: infinite CI and H0 bounds in the same direction
+  if ((delta_lb == -Inf & lb == -Inf) | (delta_ub == Inf & ub == Inf)) {
+    return(1)
+  }
 
-  correct <- max(0.5 * interval / null, 1)
+  # usual case: non-point CI & non-point Ho
+  # pdelta = |CI intersect Ho| / min{ |CI|, 2|Ho| }
+  if (delta_lb != delta_ub & lb != ub) {
+    if (lb > delta_ub | ub < delta_lb) {
+      return(0)
+    } else if(lb > delta_lb & ub < delta_ub){
+      return(1)
+    } else {
+      return(
+        (min(ub, delta_ub) - max(lb, delta_lb)) /
+          min(ub - lb, 2 * (delta_ub - delta_lb))
+      )
+    }
+  }
 
-  intersect / interval * correct
+  # special case 1: point CI, w/ or w/out a point H0
+  # pdelta = 0 if CI is inside the Ho
+  # pdelta = 1 if CI is inside the Ho
+  if (lb == ub) {
+    if (lb <= delta_ub & lb >= delta_lb){
+      return(1)
+    } else {
+      return(0)
+    }
+  }
+
+  # special case 2: point H0 & non-point CI
+  # pdelta = 1/2 if H0 is inside the CI
+  # pdelta = 0 if H0 is outside the CI
+  if (delta_lb == delta_ub & lb != ub) {
+    if (delta_lb <= ub & delta_lb >= lb) {
+      return(1/2)
+    } else {
+      return(0)
+    }
+  }
 }
