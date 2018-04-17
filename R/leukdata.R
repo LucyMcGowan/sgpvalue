3d38f7c8cfb440595b8a93b69b6c0b145e3c155c
Lucy <ld.mcgowan@vanderbilt.edu>
2018-04-16 19:24:40 -0500

    :sparkles: Add data

---
+++ R/leukdata.R
@@ -0,0 +1,16 @@
+#' Leukemia Data
+#'
+#' Data from the ALL-AML Leukemia Microarray Study used to generate
+#' Figures 3 and 4 and Table 2 in the Second-generation p-values:
+#' Improved rigor, reproducibility, & transparency in statistical analyses
+#' manuscript. A microarray analysis was conducted on 7128 candidate genes and
+#' two-group t-tests were performed on the cleaned and standardized log10
+#' expression levels of each gene.
+#'
+#' @name leukdata
+#' @format A matrix with 513216 elements
+#' @references Matejka, J., & Fitzmaurice, G. (2017).
+#' Golub TR, Slonim DK, Tamayo P, Huard C, Gaasenbeek M, Mesirov JP, et. al. (1999).
+#' Molecular classification of cancer: class discovery and class prediction by gene
+#'  expression monitoring. Science, 286(5439): 531–537. pmid:10521349
+"leukdata"
